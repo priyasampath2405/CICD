43e995c9244669eec43eb78c426e150159fd7816
Priyadharshini S <50194416+priyasampath2405@users.noreply.github.com>
2025-09-21 23:47:10 +0530

    Update Ruban

--- frontend/src/components/AppHeader.tsx
+++ frontend/src/components/AppHeader.tsx
@@ -9,7 +9,7 @@ export default function AppHeader() {
       <AppBar position="static">
         <Toolbar className="app-header">
           <Typography variant="h6" component="div" className="fg-1">
-            Priya Ruban
+            Ruban
           </Typography>
         </Toolbar>
       </AppBar>
